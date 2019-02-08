9e9d4cd9bcb57c11d7788ca579d5d33805e6a22f
Björn Tegelund <b.tegelund@gmail.com>
2019-02-08 19:29:26 +0900

    Add open project repo feature

--- README.md
+++ README.md
@@ -4,20 +4,21 @@
 
 Allows you to quickly open projects from Alfred. It will try its best to provide the best possible match for your query.
 
-### Installation
+## Installation
 
 Grab the latest release [here](https://github.com/bjrnt/alfred-project-manager/releases/) and install the workflow file.
 
 Set `PROJECT_DIRECTORY` inside the workflow's environment variables (install the workflow, select in from the workflow list, and press the `[x]` icon), and you're good to go.
 
-### Usage
+## Usage
 
 Open Alfred and type `pm` to access the project manager and try typing a query. You can also configure a hotkey for it.
 
-### Modifiers
+## Modifiers
 
-* `none` (default): open the project in VSCode
-* `alt`: open the project in Finder
-* `cmd`: open the project in iTerm
+- `none` (default): open the project in VSCode
+- `alt`: open the project in Finder
+- `cmd`: open the project in iTerm
+- `ctrl`: open the project's repo
 
 You can change which applications are used to open the project inside the workflow.
--- alfred-project-manager.go
+++ alfred-project-manager.go
@@ -4,14 +4,18 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 
-	"github.com/deanishe/awgo"
+	aw "github.com/deanishe/awgo"
 	"github.com/deanishe/awgo/fuzzy"
-	"github.com/docopt/docopt-go"
+	docopt "github.com/docopt/docopt-go"
 )
 
 var usage = `alfred-project-manager
@@ -24,10 +28,10 @@ var usage = `alfred-project-manager
     -h --help     Show this screen.`
 
 var (
-	cacheName    = "projects.json"
+	cacheName    = "projects1234.json"
 	wf           *aw.Workflow
 	fuzzyOptions []fuzzy.Option
-	maxCacheAge  = 10 * time.Minute
+	maxCacheAge  = 30 * time.Minute
 	maxResults   = 3
 )
 
@@ -39,24 +43,42 @@ func init() {
 	wf = aw.New(aw.MaxResults(maxResults), aw.SortOptions(fuzzyOptions...))
 }
 
-func getProjectDirectories() []string {
-	var projectsDirectories []string
+type Project struct {
+	Path string `json:"path"`
+	URL  string `json:"url"`
+}
+
+func getRemote(path string) (repo string, err error) {
+	cmd := exec.Command("git", "remote", "get-url", "origin")
+	cmd.Dir = path
+	out, err := cmd.Output()
+	if len(out) != 0 {
+		out := strings.TrimSpace(string(out))
+		remote := out[strings.Index(out, ":")+1 : strings.LastIndex(out, ".")]
+		repo = fmt.Sprintf("https://github.com/%s", remote)
+	}
+	return
+}
+
+func getProjects() []*Project {
+	projects := []*Project{}
 	if wf.Cache.Exists(cacheName) && !wf.Cache.Expired(cacheName, maxCacheAge) {
-		wf.Cache.LoadJSON(cacheName, &projectsDirectories)
-		return projectsDirectories
+		wf.Cache.LoadJSON(cacheName, &projects)
+		return projects
 	}
 
 	projectsPath := filepath.Join(os.Getenv("HOME"), os.Getenv("PROJECT_DIRECTORY"))
 	files, _ := ioutil.ReadDir(projectsPath)
 
-	projectsDirectories = make([]string, 0)
 	for _, file := range files {
 		if file.IsDir() {
-			projectsDirectories = append(projectsDirectories, filepath.Join(projectsPath, file.Name()))
+			path := filepath.Join(projectsPath, file.Name())
+			url, _ := getRemote(path)
+			projects = append(projects, &Project{Path: path, URL: string(url)})
 		}
 	}
-	wf.Cache.StoreJSON(cacheName, projectsDirectories)
-	return projectsDirectories
+	wf.Cache.StoreJSON(cacheName, projects)
+	return projects
 }
 
 func run() {
@@ -64,11 +86,14 @@ func run() {
 
 	query := arguments["<query>"].(string)
 
-	for _, project := range getProjectDirectories() {
-		wf.NewFileItem(project).
+	for _, project := range getProjects() {
+		log.Println(project.Path)
+		log.Println(project.URL)
+		wf.NewFileItem(project.Path).
 			Subtitle("Open in VSCode").
-			Arg(project).
-			UID(project).
+			Arg(project.Path).
+			Var("url", project.URL).
+			UID(project.Path).
 			Valid(true)
 	}
 
