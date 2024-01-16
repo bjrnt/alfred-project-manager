45f393d929ae8d48856e968f4f66619578626aee
Bjorn Tegelund <b.tegelund@gmail.com>
2024-01-16 13:12:06 -0800

    Invalidate cache based off of project dir ctime

--- cache.go
+++ cache.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"log"
+	"os"
+	"syscall"
 	"time"
 )
 
 const (
 	cacheName   = "projects.json"
-	maxCacheAge = 30 * time.Minute
+	maxCacheAge = 6 * time.Hour
 )
 
 type cache struct {
-	Params   Params
-	Projects []Project
+	Params            Params
+	ProjDirModifiedAt time.Time
+	Projects          []Project
 }
 
 // TryCache reads in any previously cached projects. The cache is invalidated if the params given
@@ -30,13 +33,28 @@ func TryCache(params *Params) []Project {
 		log.Printf("Search params do not match cached -- skipping cache")
 		return nil
 	}
+	// skip the cache if there have been changes to the project dir
+	if getModifiedAt(params.ProjectsPath()).After(cache.ProjDirModifiedAt) {
+		log.Printf("Projects have been added/removed since last caching -- skipping cache")
+		return nil
+	}
 	return cache.Projects
 }
 
 // SaveCache saves the given projects to the cache file.
 func SaveCache(params *Params, projects []Project) {
-	err := wf.Cache.StoreJSON(cacheName, cache{*params, projects})
+	err := wf.Cache.StoreJSON(cacheName, cache{*params, getModifiedAt(params.ProjectsPath()), projects})
 	if err != nil {
 		log.Printf("could not save cache: %s", err.Error())
 	}
+}
+
+func getModifiedAt(path string) time.Time {
+	dir, err := os.Stat(path)
+	if err != nil {
+		log.Printf("could not verify modified time of projects dir: %s", err.Error())
+		return time.Time{}
+	}
+	timespec := dir.Sys().(*syscall.Stat_t).Ctimespec
+	return time.Unix(timespec.Unix())
 }
